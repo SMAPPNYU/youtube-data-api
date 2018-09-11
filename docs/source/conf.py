f6eb1c36e3a128fc9c181c0d008b47794afc2ba2
yinleon <hello.leonyin@gmail.com>
2018-09-11 18:34:09 -0400

    keep reqs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
 
-MOCK_MODULES = ['pandas', "bs4", 'pytube', 'requests']
+MOCK_MODULES = ['pandas', "bs4", 'pytube']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- General configuration ------------------------------------------------
