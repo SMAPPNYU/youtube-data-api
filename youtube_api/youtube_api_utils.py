b6763869050ffb92fb799f3cb9cc8388ce442bd1
yinleon <hello.leonyin@gmail.com>
2019-02-20 14:42:14 -0500

    fixed BUG with get_subs

--- youtube_api/youtube_api_utils.py
+++ youtube_api/youtube_api_utils.py
@@ -5,6 +5,7 @@ import requests
 import html
 from bs4 import BeautifulSoup, Comment
 import re
+import signal
 
 '''
 This contains utilities used by other functions in the YoutubeDataApi class, as well as a few convenience functions for data analysis.
@@ -24,6 +25,20 @@ __all__ = [
     'get_url_from_video_id'
 ]
 
+class TimeoutError(Exception):
+    pass
+
+class timeout:
+    def __init__(self, seconds=1, error_message='Timeout'):
+        self.seconds = seconds
+        self.error_message = error_message
+    def handle_timeout(self, signum, frame):
+        raise TimeoutError(self.error_message)
+    def __enter__(self):
+        signal.signal(signal.SIGALRM, self.handle_timeout)
+        signal.alarm(self.seconds)
+    def __exit__(self, type, value, traceback):
+        signal.alarm(0)
 
 def _chunker(l, chunksize):
     """Yield successive ``chunksize``-sized chunks from l."""
